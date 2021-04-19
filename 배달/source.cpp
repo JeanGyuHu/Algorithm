f14e070c9f58435579d60c8073e0faec536c8331
허진규 <jeangyu@heojingyuui-MacBookPro.local>
2021-04-19 17:57:16 +0900

    add : 배달

---
+++ 배달/source.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define INF 1e9
+
+using namespace std;
+
+int dp[51]={INF,};
+
+int solution(int N, vector<vector<int> > road, int K) {
+    int answer = 0;
+    
+
+    
+
+    return answer;
+}
+
+int main(void){
+
+    for(int i=0; i<50; i++)
+        cout<<dp[i]<<'\n';
+
+    return 0;
+}
