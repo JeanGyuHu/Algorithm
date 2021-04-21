c38d759ce03fa3d3adfe8b1319540365178921b7
허진규 <jeangyu@heojingyuui-MacBookPro.local>
2021-04-21 17:38:26 +0900

    add : N개의 최소공배수

---
+++ N개의 최소공배수/source.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long gcd(long long a, long long b){ return b==0? a : gcd(b,a%b); }
+
+long long lcm(long long a, long long b){ return (a / gcd(a,b)) * b; }
+
+int solution(vector<int> arr) {
+    int answer = 0;
+
+    answer=lcm(arr[0], arr[1]);
+
+    for(int i=2; i<arr.size(); i++){
+        answer=lcm(answer, arr[i]);
+    }
+
+    return answer;
+}
+
+int main(void){
+
+    vector<int> v;
+
+    v.push_back(2);
+    v.push_back(6);
+    v.push_back(8);
+    v.push_back(14);
+
+    cout<<solution(v)<<'\n';
+
+    return 0;
+}
