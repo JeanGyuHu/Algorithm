a34cc600cf6b5a7a3264d89c32afa776096a56c0
허진규 <jeangyu@heojingyuui-MacBookPro.local>
2021-05-30 15:15:26 +0900

    add : 야근지수

---
+++ 야근지수/source.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long solution(int n, vector<int> works) {
+    priority_queue<int> pq;
+    long long answer = 0;
+    
+    for(int i : works) pq.push(i);
+    
+    while(n>0){
+        if(pq.top()>0){
+            int cur = pq.top();
+            pq.pop();
+            pq.push(cur-1);
+        }
+        n--;
+    }
+    
+    while(!pq.empty()) {
+        answer+=pq.top()*pq.top();
+        pq.pop();
+    }
+    
+    return answer;
+}
